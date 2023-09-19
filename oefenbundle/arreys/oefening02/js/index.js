17cbbc6d0af20ddc983467d7ca01aa9a669fbf95
natiapreutesei <natiapreutesei@gmail.com>
2023-09-19 13:31:35 +0200

    oefening02/arrays

--- oefenbundle/arreys/oefening02/js/index.js
+++ oefenbundle/arreys/oefening02/js/index.js
@@ -1,20 +1,17 @@
 let firstArray = [];
 let secondArray = [];
-// let number = parseInt(prompt("Write a number between 0 and 10:"));
-for (let i=0;i<11;i++){
+// the for that pushes all the numbers from 0 to 10 in
+for (let i=1;i<11;i++){
     firstArray.push(i);
     secondArray.push(i);
 }
-// document.write(firstArray + "<br>" +  secondArray);
+
 let sumArray  = [];
 for(let j = 0; j < firstArray.length; j++){
     sumArray.push(firstArray[j] + secondArray[j]);
 }
-
+//prints the result on screen
 for (let k=0;k<firstArray.length;k++) {
-    //document.write(`${firstArray[k]}  <br> secondArray: ${secondArray} <br>`);
-
-} document.write("Sum of the Arrays:" + sumArray);
-/*
-document.write(`${firstArray}  <br> secondArray: ${secondArray} <br>`);
-document.write("Sum of the Arrays:" + sumArray);*/
+    //document.write(firstArray[k] +" + "+ secondArray[k] + " = " + sumArray[k] + "<br>");
+     document.write(`${firstArray[k]} + ${secondArray[k]} = ${sumArray[k]} <br>`);
+}
